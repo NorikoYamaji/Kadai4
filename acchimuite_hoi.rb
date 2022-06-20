8b95823009a003ff413dde27bee291dda0cfcdac
noriko <ririnday.0215@gmail.com>
2022-06-20 08:37:54 +0000

    あっち向いてホイに移行できない

--- acchimuite_hoi.rb
+++ acchimuite_hoi.rb
@@ -1,84 +1,103 @@
-require "csv" 
+class JankenAcchimuitehoi  
+  
    puts"------------------------------"
    puts"あっち向いてホイッを始めます。"
    puts "最初はグーじゃんけん・・・・"
-
-class JankenAcchimuitehoi  #classをつけてじゃんけんの勝敗とあっち向いてホイを連動させようとしている
-                            #じゃんけんとあっち向いてホイを一かたまりにしてloopさせたい
-                            #でもエラーになる
-
-def initialize(win) 
-  @win = win
   
-def initialize(lose) 
-  @lose = lose
+def janken_acchi 
+  
+  @janken_judge = ""
 
 end
-end
 
-win = JankenAcchimuitehoi.new(win) #インスタンスメソッド作成
-lose = JankenAcchimuitehoi.new(lose)
-
-JankenAcchimuitehoi.win #インスタンスメソッドを呼び出し
-JankenAcchimuitehoi.lose
-
-
-def janken #じゃんけんの定義
+def janken
   puts "[0]グー\n[1]チョキ\n[2]パー\n[3]戦わない"
 
   my_hand = gets.to_i
-  pc_hand = rand(4)
+  pc_hand = rand(0..2)
   
   
   jankens = ["グー", "チョキ","パー","戦わない"]
   puts "あなたの手:#{jankens[my_hand]}, 相手の手:#{jankens[pc_hand]}"
 
-  if my_hand == pc_hand
-    puts "あいこで"
-    return true
+  win = (my_hand == 0 && pc_hand == 1) || (my_hand == 1 && pc_hand == 2) || (my_hand == 2 && pc_hand == 0)
+  draw = my_hand == pc_hand
+  finish = my_hand == 3
+        
+   if draw
+     puts "あいこで"
+     @janken_judge = "draw"
+        
+   elsif finish
+     puts "また今度！"
+     @janken_judge = "finish"
+     return false
+        
+   elsif win
+     puts "やったね！"
+     @janken_judge = "win"
+        
+   else 
+     puts "残念。負けちゃった。"
+     @janken_judge = "lose"
+   end  
     
-  elsif(my_hand == 3 && pc_hand == 0)||(my_hand == 3 && pc_hand == 1)||(my_hand == 3 && pc_hand == 2)||
-   (my_hand == 0 && pc_hand == 3)||(my_hand == 1 && pc_hand == 3)||(my_hand == 2 && pc_hand == 3)
-   puts"じゃんけんは後でね。"
-    return false
+   if @janken_judge == "draw"
+   janken
     
-  elsif(my_hand == 0 && pc_hand == 1)||(my_hand == 1 && pc_hand == 2)||(my_hand == 2 && pc_hand == 0)
-    puts "やったね！"
-    puts "あっち向いて…"
-    return false
-    
-  else
-    puts "負けちゃった"
-    puts "あっち向いて…"
-    return false
-  end
-end
+   #else @janken_judge == "finish"
+     
+   end
 
-next_game = true
+   next_game = true
 
-while next_game
-  next_game = janken
-end
+   while next_game = janken
+   end
 
-  def acchimuite_hoi #あっち向いてホイの定義
-    puts "[0]上\n[1]下\n[2]左\n[3]右"
-    
-    my_face = gets.to_i
-    pc_face = rand(4)
-    
-    directions = ["上", "下", "左", "右"]
-    puts "ホイ！"
-    puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
-    
-    if my_face == pc_face
-      if @win
-        puts "あっち向いてホイに勝った！"
-      else @lose
-        puts "あっち向いてホイに負けた"
-      end
+def acchimuite_hoi
+   if @janken_judge == "win"
+     puts "あっち向いてホイ"
+     puts "[0]上\n[1]下\n[2]右\n[3]左"
+     my_face = gets.to_i
+     pc_face = rand(4)
+        
+     directions = ["上", "下", "右", "左"]
+        
+   elsif my_face == pc_face
+     puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
+     puts "あっちむいてほいの勝負であなたの勝ちです。"
+        
+   else
+     puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
+     puts "あっちむいてホイの勝負がつかない。"
+     puts "じゃんけんやり戻し。"
+     janken
+            
+   end
+          
+   if @janken_judge == "lose"
+     puts "あっちむいてホイ"
+     puts "[0]上\n[1]下\n[2]右\n[3]左"
+     my_face= gets.to_i
+     pc_face = rand(4)
+        
+     directions = ["上", "下", "右", "左"]
+        
+   elsif lose_my_face == win_pc_face
+     puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
+     puts "あっち向いてホイの勝負であなたの負けです。"
+        
+   elsif
+     puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
+     puts "あっち向いてホイの勝負がつかない。"
+     puts "じゃんけんのやり戻し"
+     janken
       
-    else
-      puts "引き分け"
-    end
-  end
+   end    
+　end
+ 
 end
+end
+end
+janken_hoi_game = JankenAcchimuitehoi.new
+janken_hoi_game.janken
