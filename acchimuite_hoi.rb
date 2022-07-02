cf0b284b4942e181b5423c7b7fa813ef89641318
noriko <ririnday.0215@gmail.com>
2022-07-02 14:20:51 +0000

    じゃんけんアプリ課題提出

--- acchimuite_hoi.rb
+++ acchimuite_hoi.rb
@@ -5,15 +5,16 @@
 #じゃんけんメソッド
 class Janken_acchi
 def janken
-    @janken_result = ""
-  puts "[0]グー\n[1]チョキ\n[2]パー\n[3]戦わない"
+   @janken_result = ""
+  puts "0 (グー)、1 (チョキ)、2 (パー)、3 (戦わない)"
 
   my_hand = gets.to_i
   pc_hand = rand(0..2)
   
   jankens = ["グー", "チョキ","パー","戦わない"]
   puts "ホイッ！"
-  puts "あなたの手:#{jankens[my_hand]}, 相手の手:#{jankens[pc_hand]}"
+  puts "あなたの手:#{jankens[my_hand]}\n相手の手:#{jankens[pc_hand]}"
+  puts"------------------------------"
 
   win = (my_hand == 0 && pc_hand == 1) || (my_hand == 1 && pc_hand == 2) || (my_hand == 2 && pc_hand == 0)
   lose = (my_hand == 0 && pc_hand == 2) || (my_hand == 1 && pc_hand == 0) || (my_hand == 2 && pc_hand == 1)
@@ -22,19 +23,23 @@ def janken
         
    if draw
      puts "あいこで"
+     puts"------------------------------"
      janken
      return true
         
    elsif fin
      puts "また今度！"
+     puts"------------------------------"
      exit
         
-   elsif win || @janken_result == "win"
+   elsif win
+     @janken_result = "win"
      puts "やったね！"
      acchimuite_hoi
      return false
      
-   elsif lose || @janken_result == "lose"
+   elsif lose
+     @janken_result = "lose"
      puts "残念。負けちゃった。"
      acchimuite_hoi
      return false
@@ -43,42 +48,47 @@ end
 
 #あっち向いてほいメソッド
 def acchimuite_hoi
+  puts"------------------------------"
   puts "あっち向いて"
-  puts "[0]上\n[1]下\n[2]右\n[3]左"
-  
+  puts "0 (上)、1 (下)、2 (左)、3 (右)"
   my_face = gets.to_i
   pc_face = rand(4)
   
   directions = ["上", "下", "右", "左"]
   puts "ホイッ！"
-  puts "あなたの向き:#{directions[my_face]}, 相手の向き:#{directions[pc_face]}"
+  puts "あなたの向き:#{directions[my_face]}\n相手の向き:#{directions[pc_face]}"
+  puts"------------------------------"
   
    #勝った場合
-   if @janken_result == "win" || my_face == pc_face
+   if @janken_result == "win" && my_face == pc_face
      puts "おめでとう。あなたの勝ちです！"
+     puts"------------------------------"
      exit
   
    #負けた場合
-   elsif @janken_result == "lose" || my_face == pc_face
+   elsif @janken_result == "lose" && my_face == pc_face
      puts "残念。あなたの負けです。"
+     puts"------------------------------"
      exit
    
    #勝敗がつかない場合   
    else 
      puts "良い戦いだ！"
+     puts"------------------------------"
      puts "じゃんけん"
      janken
      return true
    end
 end
+
 end #class janken_acchiのend
 
-janken_acchi = Janken_acchi.new("win","lose")
-janken_acchi.janken()
-janken_acchi.acchimuite_hoi()
+janken_acchi = Janken_acchi.new()
+janken_acchi.janken
+janken_acchi.acchimuite_hoi
 
- next_game = true
+next_game = true
    
    while next_game do
-     next_game = janken
+     next_game = janken_acchi
    end
